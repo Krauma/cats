7565cdec4de031c845226fedfae541cc35698ec1
Henrik Kruger <henrikkrugerjensen@gmail.com>
2022-01-22 13:49:50 +0100

    done with voting

--- cats/lib/Exploration/Views/exproration_screen_top_part.dart
+++ cats/lib/Exploration/Views/exproration_screen_top_part.dart
@@ -1,5 +1,6 @@
 import 'package:cats/Exploration/Controllers/exploration_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'cat_container.dart';
 
@@ -16,11 +17,21 @@ class ExplorationSreenTopPart extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Explore kittens',
-              style: GoogleFonts.outfit(
-                  color: Colors.white,
-                  fontSize: 26,
-                  fontWeight: FontWeight.w400)),
+          Row(
+            children: [
+              IconButton(
+                  onPressed: Get.back,
+                  icon: const Icon(
+                    Icons.close,
+                    color: Colors.white,
+                  )),
+              Text('Explore kittens',
+                  style: GoogleFonts.outfit(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.w400)),
+            ],
+          ),
           CatContailer(explorationController: explorationController),
         ],
       ),
